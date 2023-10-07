f032b8160409c6f609b5b777a8ea92dd08872e1e
brunosgatti <brunogatti@usp.br>
2023-10-07 14:06:20 -0300

    added Q10 script

---
+++ Python4_10.2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+list = [x for x in range(int(input('Insira o valor inicial:', )),int(input('Insira o valor final:', ))) if x % 2 == 1]
+print(list)
+
+
+
---
+++ Python4_10.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+list = [x for x in range(int(input('Insira o valor inicial:', )),int(input('Insira o valor final:', )))]
+print(list)
+
+#No enunciado fala que posso salvar o script como 'count.py 3 10' para que esses sejam os valores iniciais e finais, mas como inicialmente o pedido é para que os valores sejam dados pela linha de comando, não salvarei o arquivo com esse nome.
